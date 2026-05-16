ea9ba595579a2c37966ed127b21ee2a17553ae39
Maria Eduarda Verissimo <duardaverissimo@gmail.com>
2026-05-16 19:50:12 -0300

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,15 @@ function abrirModal(){
     document.querySelector(".fechar").onclick = function() {
   document.getElementById("modalAviso").style.display = "none";
 }
+}
 
-
-
+function contato(){
+    //para ativar o modal, temos que por display "block" lembrando que tem que ser a div maior
+    document.getElementById("modalContato").style.display="block";
+    //com id que criamos para vc fechar o model, criamos a funcao para fechar o modal e aparecer oq texto que colocamos, no caso o
+    // "modalContato" que no caso é a div maior
+    document.getElementById("fecharcntt").onclick=function(){
+    //exibe o fechar do modal e na funcao exibe o mmodal-conteudo
+        document.getElementById("modal-conteudo").style.display="none";
+    }
 }
