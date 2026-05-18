7d1c8510df0cba890f494acdadb17b0edc6f338a
Maria Eduarda Verissimo <duardaverissimo@gmail.com>
2026-05-18 20:10:09 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,10 +2,15 @@
 
 function abrirModal(){
     //para ativar o modal, temos que por display "block" lembrando que tem que ser a div maior
-    document.getElementById("modalAviso").style.display = "block";
+    
     //criamos uma funcao para fechar o modal e aparecer oq texto que colocamos, no caso o "modalAviso"
+   document.getElementById("modalAviso").style.display = "flex";
+
+
     document.querySelector(".fechar").onclick = function() {
   document.getElementById("modalAviso").style.display = "none";
+ 
+ 
 }
 }
 
